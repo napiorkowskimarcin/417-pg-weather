bf373107202b0bbc5808f945f9294e292add008c
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2021-03-08 08:59:25 +0100

    get api weather

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+     private $client;
+
+    public function __construct(HttpClientInterface $client) 
+    {
+        $this->client = $client;
+    }
     /**
       * @Route("/home")
       */
     public function index(): Response
     {
-       return $this->render('home.html.twig');
+        $response = $this->client->request(
+            'GET',
+            'http://api.gios.gov.pl/pjp-api/rest/station/findAll'
+        );
+         $content = $response->getContent();
+        $content = $response->toArray();
+
+        return $this->render('home.html.twig', ['content'=>$content]);
     }
 }
